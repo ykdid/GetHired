b640f27db4c2a8013678ccfe5fecc1c5a3e65f74
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-08-27 09:51:48 +0300

    PasswordChangeModal bug fixed, new Endpoints added (changePassword) and Services updated.

---
+++ server/RecruitmentAPI/DTOs/Models/PasswordChangeModel.cs
@@ -0,0 +1,7 @@
+namespace RecruitmentAPI.Services.EmployerService.DTOs;
+
+public class PasswordChangeModel
+{
+    public string CurrentPassword { get; set; }
+    public string NewPassword { get; set; }
+}
---
+++ server/RecruitmentAPI/Migrations/20240820071824_IncreaseEmployerImagePathLength.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecruitmentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseEmployerImagePathLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployerImagePath",
+                schema: "backoffice",
+                table: "Employer",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployerImagePath",
+                schema: "backoffice",
+                table: "Employer",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
