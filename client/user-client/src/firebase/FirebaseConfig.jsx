89bc359b35864c8f9130dda4a10114ff628a85b6
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2024-09-10 04:44:14 +0300

    edit: firebaseconfig

--- client/user-client/src/firebase/FirebaseConfig.jsx
+++ client/user-client/src/firebase/FirebaseConfig.jsx
@@ -3,13 +3,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:  process.env.REACT_APP_FIREBASE_APP_ID,
-  //measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD3uQ7CnwpLz0HqZwcGNKCCeOwCB5oRi8Q",
+  authDomain: "gethired-new.firebaseapp.com",
+  projectId: "gethired-new",
+  storageBucket: "gethired-new.appspot.com",
+  messagingSenderId: "201337867633",
+  appId: "1:201337867633:web:0f821e02c22e12a8a97ad0"
 };
 
 
